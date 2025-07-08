80f8ff011346d3e40c1559ff2d5d86ae7e25908a
Etienne Gonthier Lapointe <etienne.gonthier-lapointe@codeboxx.biz>
2025-07-08 16:21:35 -0400

    feat: frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="w-full p-6">
+    <div className="w-full p-6 bg-gray-50 min-h-screen">
       <Navbar />
       <Outlet />
     </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import RecordList from './components/RecordList.jsx'
+import Record from './components/Record.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <RecordList />
+      },
+      {
+        path: "/create",
+        element: <Record />
+      },
+      {
+        path: "/edit/:id",
+        element: <Record />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
